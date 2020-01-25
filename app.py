19df9cf3dbae456fac8e513d29b84e914a06ad22
AngelWingJasmineT <kterada0322@gmail.com>
2020-01-25 12:42:57 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('index.html',title="Index with Jinja",message="これはJinjaテンプレートの利用例です。！")
+
+@app.route('/<id>/<password>')
+def index2(id, password):
+  msg = 'id: %s, password: %s' % (id, password)
+  return render_template('index.html',title="Index with Jinja",message=msg)
